a8e61b1632f83f429b4bc453a3ce04cf5adb7682
Om Parihar <Omshivoham191@gmail.com>
2026-09-06 18:25:25 +0530

    added getMe and updateProfile routes

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,18 +1,13 @@
 import express from "express";
-import { registUser, loginUser } from "../controllers/auth.controller.js";
+import { registUser, loginUser, getMe, updateProfile } from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register",registUser);
-
 router.post("/login", loginUser);
 
-router.get("/me", protect, (req,res)=>{
-    res.json({
-        success: true,
-        user: req.user
-    });
-});
+router.get("/me", protect, getMe);
+router.put("/profile", protect, updateProfile);
 
 export default router;
