39d825b5c5e8113bb8119db7f8db28fedd217df8
Om Parihar <Omshivoham191@gmail.com>
2026-09-07 22:10:58 +0530

    added getProject routes

--- Backend/routes/project.routes.js
+++ Backend/routes/project.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { createProject  } from "../controllers/project.controller.js";
+import { createProject, getProjects  } from "../controllers/project.controller.js";
 
 const router = express.Router();
 
 router.post("/",protect, createProject);
+
+router.get("/", protect, getProjects);
 
 export default router;
